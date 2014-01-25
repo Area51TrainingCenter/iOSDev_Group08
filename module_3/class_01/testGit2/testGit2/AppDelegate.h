483c950b7839f9bc325293ca90cd39e70b3036cf
frantisan <frantisaul@gmail.com>
2014-01-25 09:16:40 -0500

    Initial Commit

---
+++ module_3/class_01/testGit2/testGit2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testGit2
+//
+//  Created by Franti Saúl Huamán Mera on 1/25/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ module_3/class_01/testGit2/testGit2Tests/testGit2Tests.m
@@ -0,0 +1,34 @@
+//
+//  testGit2Tests.m
+//  testGit2Tests
+//
+//  Created by Franti Saúl Huamán Mera on 1/25/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testGit2Tests : XCTestCase
+
+@end
+
+@implementation testGit2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
