a5bd5897cc5da12c255243fb1e1f2ff7c3ca3568
frantisan <frantisaul@gmail.com>
2014-01-28 21:58:14 -0500

    Creando nuestro primer UIActionSheet

--- module_3/class_02/CalculadoraIBActionIBOutlet/CalculadoraIBActionIBOutlet/ViewController.m
+++ module_3/class_02/CalculadoraIBActionIBOutlet/CalculadoraIBActionIBOutlet/ViewController.m
@@ -30,6 +30,7 @@
     self.resultado.text = [NSString stringWithFormat:@"%i",[self.cajaUno.text intValue]+[self.cajaDos.text intValue]];
     
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Suma" message:self.resultado.text delegate:self cancelButtonTitle:@"Cancelar" otherButtonTitles:@"Aceptar", nil];
+    alert.tag = 1;
     [alert show];
 }
 - (IBAction)dividir:(id)sender {
@@ -38,21 +39,29 @@
 
 - (IBAction)restar:(id)sender {
     self.resultado.text = [NSString stringWithFormat:@"%i",[self.cajaUno.text intValue]-[self.cajaDos.text intValue]];
+    
+    UIActionSheet *t = [[UIActionSheet alloc] initWithTitle:@"Resultado" delegate:self cancelButtonTitle:@"Cancelar" destructiveButtonTitle:@"Elminar" otherButtonTitles:@"Botón 1", @"Botón 2", nil];
+    [t showInView:self.view];
 }
 
 - (IBAction)multiplicar:(id)sender {
     self.resultado.text = [NSString stringWithFormat:@"%i",[self.cajaUno.text intValue]*[self.cajaDos.text intValue]];
 }
 
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    
+}
+
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
     
-    if (buttonIndex==0) {
-        NSLog(@"Canceló");
-    }else{
-        self.view.backgroundColor = [UIColor greenColor];
-        NSLog(@"Cualquier otro botón");
+    if (alertView.tag==1) {
+        if (buttonIndex==0) {
+            NSLog(@"Canceló");
+        }else{
+            self.view.backgroundColor = [UIColor greenColor];
+            NSLog(@"Cualquier otro botón");
+        }
     }
-    
 }
 
 @end
