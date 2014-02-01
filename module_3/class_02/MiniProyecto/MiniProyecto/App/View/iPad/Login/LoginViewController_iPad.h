c2866882730212fa08efaadb12a5f2e9c929e780
frantisan <frantisaul@gmail.com>
2014-02-01 11:30:52 -0500

    Login en iPad

--- module_3/class_02/MiniProyecto/MiniProyecto/App/View/iPad/Login/LoginViewController_iPad.h
+++ module_3/class_02/MiniProyecto/MiniProyecto/App/View/iPad/Login/LoginViewController_iPad.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LoginViewController_iPad : UIViewController
+@interface LoginViewController_iPad : UIViewController<UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *loginTableView;
 
 @end
--- module_3/class_02/MiniProyecto/MiniProyecto/App/View/iPad/Login/LoginViewController_iPad.m
+++ module_3/class_02/MiniProyecto/MiniProyecto/App/View/iPad/Login/LoginViewController_iPad.m
@@ -8,31 +8,52 @@
 
 #import "LoginViewController_iPad.h"
 
+//custom cells
+#import "LoginCell.h"
+
 @interface LoginViewController_iPad ()
 
 @end
 
 @implementation LoginViewController_iPad
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+#pragma mark -
+#pragma mark LifeCycle Methods
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
     }
     return self;
 }
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark -
+#pragma mark Table View Data Source Methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 2;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    LoginCell *l = [tableView dequeueReusableCellWithIdentifier:@"formCell" forIndexPath:indexPath];
+    if (indexPath.row==0) {
+        l.nombre.text = @"Email";
+        l.valor.placeholder = @"";
+    }else{
+        l.nombre.text = @"Password";
+        l.valor.placeholder = @"";
+    }
+    return l;
+}
+
 
 @end
