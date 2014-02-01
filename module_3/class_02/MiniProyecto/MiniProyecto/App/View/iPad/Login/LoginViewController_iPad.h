5615d113178d398558deb6928ad0444aba524ec8
frantisan <frantisaul@gmail.com>
2014-02-01 10:32:00 -0500

    Login en iPad y iPhone

---
+++ module_3/class_02/MiniProyecto/MiniProyecto/App/View/iPad/Login/LoginViewController_iPad.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController_iPad.h
+//  MiniProyecto
+//
+//  Created by Franti Saúl Huamán Mera on 2/1/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController_iPad : UIViewController
+
+@end
